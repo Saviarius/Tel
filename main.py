a6ba369f101027e1d5cf11e47842e666beec08f4
Saviarius <saviarius@gmail.com>
2020-12-25 22:58:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from telethon import TelegramClient, events
+
+# Remember to use your own values from my.telegram.org!
+api_id = 2922002
+api_hash = 'b3e3d448e91dccb8e175696a182b28cd'
+
+client = TelegramClient('first', api_id, api_hash)
+
+
+@client.on(events.NewMessage())
+async def handler(event):
+    try:
+        if event.message.media.document.attributes[0].voice:
+            await event.delete()
+            print(event.stringify())
+            print(event.message.peer_id.user_id)
+            await client.send_file(event.message.peer_id.user_id, 'C:/Users/Favoryt/Desktop/lol.jpg')
+    except AttributeError:
+        pass
+
+
+client.start()
+client.run_until_disconnected()
